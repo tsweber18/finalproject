05b0d5e78291d8052b4f05b17308f8a2f625f61c
Tim Weber <tim.weber@aggiemail.usu.edu>
2017-04-25 20:18:46 +0000

    Working on basic testing model

--- storyteller/models.py
+++ storyteller/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Nouns(models.Model):
+    name = models.CharField(max_length=20)
+    homeworld = models.CharField(max_length=20)
+    shipName = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+        
