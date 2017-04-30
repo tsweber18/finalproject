f99b4316afa7cf89de6c03ab24d3b314d1612d8e
Tim Weber <tim.weber@aggiemail.usu.edu>
2017-04-30 18:48:49 +0000

    Made a ModelForm, cosmetics

--- storyteller/models.py
+++ storyteller/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.forms import ModelForm
 
 class Nouns(models.Model):
     name = models.CharField(max_length=20)
@@ -9,3 +10,6 @@ class Nouns(models.Model):
     def __str__(self):
         return self.name
         
+class NounsForm(ModelForm):
+    model=Nouns
+    fields = ['name','homeworld','shipName']
