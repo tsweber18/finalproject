cdd0380f8cf02cb9345da6c8676dd83588ab5922
Tim Weber <tim.weber@aggiemail.usu.edu>
2017-05-01 03:34:47 +0000

    Lots of finagling to make things behave in a somewhat neat way.

--- storyteller/static/storyteller/scripts.js
+++ storyteller/static/storyteller/scripts.js
@@ -1,29 +1,27 @@
-var ship1string=`Congratulations to {{a.name}}! Due to the expedience in applying (rather than us having to draft them), they've been selected to serve on a Maelstrom-class
-    battleship out on the new battlefront against the disgusting, revolting, and ferocious Tyranid swarm!  It's a very prestigious post, but they've selected to take out the
-    {{a.shipName}}, which is a hearty vessel that will take them far and wide.  They'll see and obliterate worlds far less beautiful and useful than {{a.homeworld}}.  Don't worry,
-    they're just a bunch of bugs. They're ugly and don't have feelings.  Thanks again for your service, {{a.name}}!  Now hurry up and get out there before they get us first!`;
 
-var ship2string=`{{b.name}}! Get all the social contact that you can, because you've been assigned to the long and luxurious bout of loneliness that comes with outer space patrol!
-    You'll be on your faithful Rifter-class frigate called the {{b.shipName}}, and it will be your only source of company for the next few months.  Or years.  Don't worry, the
+var ship1string=`Congratulations to `+aname+`! Due to the expedience in applying (rather than us having to draft them), they've been selected to serve on a Maelstrom-class
+    battleship out on the new battlefront against the disgusting, revolting, and ferocious Tyranid swarm!  It's a very prestigious post, but they've selected to take out the
+    `+aship+`, which is a hearty vessel that will take them far and wide.  They'll see and obliterate worlds far less beautiful and useful than `+aworld+`.  Don't worry,
+    they're just a bunch of bugs. They're ugly and don't have feelings.  Thanks again for your service, `+aname+`!  Now hurry up and get out there before they get us first!`;
+
+var ship2string= bname+`! Get all the social contact that you can, because you've been assigned to the long and luxurious bout of loneliness that comes with outer space patrol!
+    You'll be on your faithful Rifter-class frigate called the `+bname+`, and it will be your only source of company for the next few months.  Or years.  Don't worry, the
     on-board voice isn't too robotic anymore and can talk about such exciting topics as advanced math and plot coursing.  You'll likely never communicate with a human while you're
-    out there unless something bad is happening.  I hope you didn't like it much on {{b.homeworld}}, because on their return, they'll like it a lot more!`;
+    out there unless something bad is happening.  I hope you didn't like it much on `+bworld+`, because on their return, they'll like it a lot more!`;
     
-var ship3string=`We're hoping that {{c.name}} is a lover of industry, because he's out to get us the much needed resources that our expanding civilization requires.  Having long since
-    depleted the most useful resources on {{c.homeworld}}, it was decided that all those joining the Imperial Navy from there should be sent to get...well...more!  They'll be on a hardy
-    but totally vulnerable Retriever-class mining vessel called the {{c.shipName}}, looking out in the scattered and lonely asteroid belts for things that will make us rich and make them
-    a great contributor to our success.  Thanks for your level of commitment to refining and manufacturing, {{c.name}}!`;
+var ship3string=`We're hoping that `+cname+` is a lover of industry, because he's out to get us the much needed resources that our expanding civilization requires.  Having long since
+    depleted the most useful resources on `+cworld+`, it was decided that all those joining the Imperial Navy from there should be sent to get...well...more!  They'll be on a hardy
+    but totally vulnerable Retriever-class mining vessel called the `+cship+`, looking out in the scattered and lonely asteroid belts for things that will make us rich and make them
+    a great contributor to our success.  Thanks for your level of commitment to refining and manufacturing, `+cname+`!`;
 
 function ship1(){
-    document.getElementById('storyPicture').innerHTML = '<img src={% static "storyteller/images/Ship1.jpg" %}';
     document.getElementById('storyOutput').innerHTML = ship1string;
 }
 
 function ship2(){
-    document.getElementById('storyPicture').innerHTML = '<img src={%static "storyteller/images/Ship2.jpg"%}';
     document.getElementById('storyOutput').innerHTML = ship2string;
 }
 
 function ship3(){
-    document.getElementById('storyPicture').innerHTML = '<img src={%static "storyteller/images/Ship3.jpg"%}';
     document.getElementById('storyOutput').innerHTML = ship3string;
 }
--- storyteller/views.py
+++ storyteller/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.views import generic
 from django.http import HttpResponseRedirect,Http404,HttpResponse
-from .models import Nouns, NounsForm
+from storyteller.models import Nouns, NounsForm
 
 def index(request):
     if request.POST:
-        Nouns = NounsForm(request.POST)
-        if Nouns.is_valid():
-            Nouns.save()
-    return render(request, 'storyteller/main.html', {'Nouns': Nouns})
+        form = NounsForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'storyteller/main.html')
     
 def ship(request):
-    a=Nouns.objects.get(pk=1)
-    b=Nouns.objects.get(pk=2)
-    c=Nouns.objects.get(pk=3)
-    return render(request, 'storyteller/ship.html', {'a': a}, {'b': b}, {'c': c})
+    list=Nouns.objects.all()
+    a=list[0]
+    b=list[1]
+    c=list[2]
+    context={
+        'a': a,
+        'b': b,
+        'c': c,
+    }
+    return render(request, 'storyteller/ship.html', context)
